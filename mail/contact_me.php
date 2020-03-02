9471e31a04c3b88bd2ce3e53591227341f065f29
gfuerte <gregorio.fuerte@gmail.com>
2020-03-02 16:24:43 -0500

    version 1.4.1: php working?

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,12 +17,7 @@ $body = "You have received a new message from your website contact form.\n\n"."H
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
-$mail=mail($to, "Subject: $email_subject",$message );
-if($mail){
-echo "Thank you for using our mail form";
-}else{
-echo "Mail sending failed.";
-}
-//if(!mail($to, $subject, $body, $header))
-//  http_response_code(500);
-//?>
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
